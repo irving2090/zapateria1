e9d0db727c50cc57714c62350bd7271d0d996323
irving2090 <irving2090@hotmail.com>
2017-06-30 15:11:48 -0500

    Update prueba.java

--- zapateria/src/zapateria/proyectointerfaces/prueba.java
+++ zapateria/src/zapateria/proyectointerfaces/prueba.java
@@ -11,6 +11,6 @@ package zapateria.proyectointerfaces;
  */
 public class prueba {
     public static void main(String[] args) {
-        System.out.println("HOLA MUNDO");
+        System.out.println("PANTOJA");
     }
 }
